23709dd882b8811b40d55b9ceb8f4f1c3858bd01
joe-sousa <joe_sousa@alu.ufc.br>
2025-11-10 21:40:42 -0300

    docs(terraform): revisa e aprimora explicações nos outputs para facilitar entendimento

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,25 +1,40 @@
-# já existiam:
+#############################################
+# Outputs – Terraform (Projeto AWS OCR)
+# Exibe informações importantes após o apply
+# Facilita integração com scripts e debugging
+#############################################
+
+# ------------------------
+# Recursos principais
+# ------------------------
 output "s3_bucket"     { value = aws_s3_bucket.images.bucket }
 output "dynamo_table"  { value = aws_dynamodb_table.crud_logs.name }
 output "sqs_queue_url" { value = aws_sqs_queue.jobs.url }
 
-# novo: região do provider em uso
+# ------------------------
+# Metadados da Região
+# ------------------------
 output "aws_region" {
   value = data.aws_region.current.name
 }
 
-# para usar o output acima, adicione este data source em main.tf:
+# Observação:
+# Para que o output acima funcione, é necessário
+# adicionar no main.tf o data source:
 # data "aws_region" "current" {}
 
-# (quando criarmos o RDS, adicionaremos:)
-# output "rds_endpoint" { value = aws_db_instance.ocr.endpoint }
-# output "rds_db_name"  { value = aws_db_instance.ocr.db_name }
-
-output "web_public_ip"     { value = aws_instance.web.public_ip }
-output "worker_public_ip"  { value = aws_instance.worker.public_ip }
+# ------------------------
+# Banco de Dados (RDS)
+# ------------------------
 output "rds_endpoint"      { value = aws_db_instance.rds.address }
 output "rds_db_name"       { value = aws_db_instance.rds.db_name }
 output "rds_user" {
   value = var.db_user
 }
+
+# ------------------------
+# Endereços das Instâncias EC2
+# ------------------------
+output "web_public_ip"     { value = aws_instance.web.public_ip }
+output "worker_public_ip"  { value = aws_instance.worker.public_ip }
 
